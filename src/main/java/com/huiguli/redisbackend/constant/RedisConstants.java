7e3eccb3be0c18c73c3da4b5be7b1c7472607aa7
huiguli <847819010@qq.com>
2023-04-24 23:53:33 +0800

    定义 Redis 常量

---
+++ src/main/java/com/huiguli/redisbackend/constant/RedisConstants.java
@@ -0,0 +1,9 @@
+package com.huiguli.redisbackend.constant;
+
+/**
+ * @author huiguli
+ */
+public class RedisConstants {
+    public static final String LOGIN_CODE_KEY = "login:code:";
+    public static final Long LOGIN_CODE_TTL = 2L;
+}
